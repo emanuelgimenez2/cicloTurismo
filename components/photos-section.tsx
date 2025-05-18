c4a706de23026f3faeabb47a93b9870b8dd3705a
Iarii <iaricolombo8@gmail.com>
2025-05-18 00:30:53 -0300

    footer y visual

--- components/photos-section.tsx
+++ components/photos-section.tsx
@@ -12,27 +12,27 @@ import { useFirebaseContext } from "@/lib/firebase/firebase-provider"
 const defaultPhotos = [
   {
     id: "photo-1",
-    url: "/foto 1.jpg?height=300&width=300",
+    url: "/foto 1 pc.jpg?height=300&width=300",
     description: "Participantes en la línea de salida",
   },
   {
     id: "photo-2",
-    url: "/foto 2.jpg?height=300&width=300",
+    url: "/foto 2 pc.jpg?height=300&width=300",
     description: "Recorriendo los paisajes de Entre Ríos",
   },
   {
     id: "photo-3",
-    url: "/foto 3.jpg?height=300&width=300",
+    url: "/foto 3 pc.jpg?height=300&width=300",
     description: "Parada de hidratación",
   },
   {
     id: "photo-4",
-    url: "/foto 4.jpg?height=300&width=300",
+    url: "/foto 4 pc.jpg?height=300&width=300",
     description: "Llegada a las termas",
   },
   {
     id: "photo-5",
-    url: "/foto 5.jpg?height=300&width=300",
+    url: "/foto 5 pc.jpg?height=300&width=300",
     description: "Entrega de premios",
   },
 ]
